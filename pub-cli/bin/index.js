56cc0a2154f10c92721cfff9cfcab64f0a0cb78a
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-06-04 17:41:22 +0200

    developpement pub cli

---
+++ pub-cli/bin/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+var program =require('commander');
+program.version('1.0.0').option('-l, --list','Get the pubs list' ).parse(process.argv);
+if(program.list) {
+    require('../src/main').getListPub();
+}
