f0b5dc06a5788939fa441db74212c1f57f12fbf1
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-06-04 16:33:54 +0200

    service des pub OK (no test)

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var pubService = require('./services/pubs.service');
+var pubService = require('./services/pub-services');
 module.exports = {
     services:{
         pubService:pubService
     }
 }
+
+
+console.log("liste de tout les pubs")
+console.log(pubService.listeDePub());
+
+console.log("liste de tout les pubs ouvert aujourd'hui")
+console.log(pubService.listeDePubOuvert());
